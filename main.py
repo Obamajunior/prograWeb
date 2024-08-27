78e707daa237428480a820bb09f75fecc19fc8cf
Gerardo sanchez gonzalez <gerardo.sanchez.con@gmail.com>
2024-08-27 11:46:34 -0600

    archivo de prueba

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hola")
+
+#Git Bash
+#ls = list Directory
+
+#ls -la = muestra todo junto con los git
+
+#git status = muestra todos los archivos que no se han agregado
+
+#git add . = metera todo al git
